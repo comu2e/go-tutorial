49bd3b214d1a036b1527eb67dbffe21f75bb8adb
英士 <comu2e@gmail.com>
2021-03-21 19:13:12 +0900

    試験合格したか確認するプログラム

--- main.go
+++ main.go
@@ -2,82 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
-	//print
-	fmt.Println("Hello")
-	fmt.Println("Good")
-	//変数の設定
-	name := 23;
-	fmt.Println(name)
-	//条件分岐
-	if name == 2 {
-		fmt.Println("True");
-	//elseは改行したらエラー
-	} else {fmt.Println("False")}
-
-	//配列
-	a := [2][2]string{{"satp","suzuki"},{"takahashi","tanaka"}}
-
-	fmt.Println(a[1][0])
-
-	age := 22
-
-	if age >= 20{
-		fmt.Println("adult")
-	}else if age == 0{
-		fmt.Println("baby")
-	}else{
-		fmt.Println("child")
-	}
-	//for文　１つめ
-	for i := 0;i<=age;i++{
-		fmt.Println(i);
-	}
-	//for文　書き方２つめ
-	i :=0
-	for i<= age {
-		fmt.Println(i)
-		i++;
-	}
-//	for文　書き方３つめ
-	j :=0
-
-	for {
-		fmt.Println(j)
-		if j ==10{
-			break
-		}
-		j++;
-	}
-
-	fmt.Println(multiple_by_3(20));
-
-	var s Student
-	s.name = "takashi"
-	s.math = 80
-	s.english = 90
-	fmt.Println(s)
-	s2 := Student{"kato",90,20}
-	fmt.Println(s2)
-	var u User
-	u.age = 20
-	u.gender = "man"
-	fmt.Println(u)
-}
-
-func multiple_by_3(i int) int {
-	fmt.Println("掛け算");
-	return (i*3)
-}
-
-//構造体
 type Student struct {
 	name string
-	math float64
-	english float64
 }
 
-type User struct{
-	gender string
-	age int
+func (s Student) calAvg(data []float64) (avgResult float64) {
+	sum := 0.0
+
+	for i := 0 ; i< len(data);i++{
+		sum += data[i]
+	}
+	result := sum/float64(len(data))
+	return result
+}
+
+func (s Student) judge(avg float64) (judgeResult string){
+	if avg >= 60{
+		judgeResult = "passed"
+	} else{
+		judgeResult = "failed"
+	}
+	return judgeResult
+}
+
+func main()  {
+	s := Student{"eiji"}
+	data := []float64{40,100,100,100,30}
+
+	avg := s.calAvg(data)
+	result := s.judge(avg)
+	fmt.Println(result)
 }
